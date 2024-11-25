084822e060abbc31b59109e21452e07817b200e2
현진 노 <noh0605@naver.com>
2024-11-25 09:29:39 +0900

    배포 전 찐찐막 디자인 수정

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
       className="header"
       onClick={() => (window.location.href = "/")} // 루트로 이동
     >
-      <h1>The Ajou Globe</h1>
+      <h1>Articles Page</h1>
       <h1>This is the test page, more articles will be uploaded soon.</h1>
     </header>
   );
