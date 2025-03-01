e5e2f8c62163922d503331fd77685df2cbd98a60
HyunjinNoh <noh0605@ajou.ac.kr>
2025-03-01 16:48:31 +0900

    postForm UI 수정

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -159,7 +159,7 @@ function PostForm() {
           value={form.page}
           onChange={(e) => setForm({ ...form, page: e.target.value })}
         >
-          <option>(예: 3면(1)이면 31 선택)</option>
+          <option>(예: 3면(1)이면 31 선택, 1면이면 11 선택)</option>
           <option>11</option>
           <option>12</option>
           <option>13</option>
@@ -199,10 +199,10 @@ function PostForm() {
         </select>
       </div>
       <div>
-        <label>20 words for preview</label>
+        <label>1~2 sentences for preview</label>
         <input
           type="text"
-          placeholder="(예: Samsung Electronics’ stock price, which stood at 83,100 won on August 1, ...) "
+          placeholder="(예: Samsung Electronics’ stock price, which stood at 83,100 won on August 1, plunged by nearly 40 percent to 49,900 won by November 14—the lowest price since May 2020.) "
           value={form.previewSentence}
           onChange={(e) => setForm({ ...form, previewSentence: e.target.value })}
         />
@@ -211,7 +211,7 @@ function PostForm() {
         <label>1 Thumbnail Image Link</label>
         <input
           type="text"
-          placeholder="에디터로 사진 업로드 후 링크 복사 붙여넣기"
+          placeholder="에디터로 사진 업로드 후 링크크 복사 붙여넣기 (예: https://theajouglobe.s3.ap-northeast-2.amazonaws...jpg)"
           value={form.thumbnailImage}
           onChange={(e) => setForm({ ...form, thumbnailImage: e.target.value })}
         />
@@ -223,12 +223,15 @@ function PostForm() {
             <a href="https://www.iloveimg.com/ko/compress-image" target="_blank">  iloveimg(다른 곳도 가능)</a><br></br>
           💚사진명 - 숫자만! 예) 173호 3면(1) 보도사진1 ▶ "173_3(1)_1"<br></br>
           💚원하는 위치에 갤러리 아이콘 클릭해서 "업로드 메뉴"▶"파일 선택", 서버로 전송" 클릭▶이미지 너비 400입력▶확인 클릭<br></br>
-          💚사진설명 - 글씨체: 기본, 글씨크기: 11 pt, 정렬: 왼쪽, 글씨스타일: 볼드<br></br><br></br>
+          💚사진설명 - 글씨체: 기본, 글씨크기: 9 pt, 정렬: 왼쪽, 글씨스타일: 볼드<br></br><br></br>
           ※텍스트 규칙※<br></br>
-          💚기사내용 - 글씨체: times new roman, 글씨크기: 14 pt, 정렬: 양쪽<br></br>
-          💚기자명 - 한 줄 띄고, 글씨체: 기본, 글씨크기: 14 pt, 정렬: 오른쪽<br></br>
+          💚기사내용 - 글씨체: times new roman, 글씨크기: 기본, 정렬: 양쪽<br></br>
+          💚기자명 - 한 줄 띄고, 글씨체: 기본, 글씨크기: 기본, 정렬: 오른쪽<br></br>
           💚그 외 각주, 이탤릭, 볼드 등 다듬기<br></br><br></br>
           ※주의사항※<br></br>
+          ❤️썸네일은 업로드한 사진 "링크 복사"해서 링크 붙여넣으시거나, <br></br>
+            기사에 올리고 싶진 않은데 썸네일로만 넣고 싶으시다면 "업로드 메뉴"▶"파일 선택", 서버로 전송"까지만 하고 링크만 복붙하셔도 됩니다.<br></br>
+            그대신 압축은 그대로 + 사진명은 "173_3(1)_thumbnail" 이런 식으로 작성해주세요! <br></br>
           ❤️제목은 content칸에 또 쓰지 않기!<br></br>
           ❤️붙여넣기 할 때 ctrl+ shift + v or "텍스트로 붙여넣기" 버튼<br></br>
           ❤️탭 대신 스페이스 4번 or "들여쓰기" 버튼<br></br>
